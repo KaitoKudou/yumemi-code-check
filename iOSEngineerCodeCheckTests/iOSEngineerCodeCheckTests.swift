bf84d9d7e94abb2a51fe26f34f359c75af5414e9
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-06-28 12:33:21 +0900

    [Add]テスト追加

--- iOSEngineerCodeCheckTests/iOSEngineerCodeCheckTests.swift
+++ iOSEngineerCodeCheckTests/iOSEngineerCodeCheckTests.swift
@@ -10,6 +10,8 @@ import XCTest
 @testable import iOSEngineerCodeCheck
 
 class iOSEngineerCodeCheckTests: XCTestCase {
+    private(set) var countOfUpdateRepositories = 0
+    private var repositories: [Item] = []
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
+++ iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
@@ -7,29 +7,33 @@
 //
 
 import XCTest
+//@testable import iOSEngineerCodeCheck
 
 class iOSEngineerCodeCheckUITests: XCTestCase {
-
+    let app = XCUIApplication()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let searchField = app.searchFields.allElementsBoundByIndex.first
+        XCTAssert(searchField?.exists ?? false)
+        searchField?.tap()
+        searchField?.typeText("Swift")
+        app.buttons["Search"].tap()
+        
+        let cell = app.staticTexts["apple/swift"]
+        XCTAssert(cell.waitForExistence(timeout: 5))
+        cell.tap()
     }
 
     func testLaunchPerformance() throws {
